fbaa3cdb919277823c75b56ddf0950a0603c9d99
Krishnastm63 <krishnastm63@gmail.com>
2023-06-29 13:40:33 +0530

    first commit

---
+++ Basic/sum-number-1.java
@@ -0,0 +1,10 @@
+class SumNumber {
+    public static void main(String[] args) {
+        int number = 7;
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
---
+++ Basic/sum-number-2.java
@@ -0,0 +1,7 @@
+class SumNumber2 {
+    public static void main(String[] args) {
+        int number = 7;
+        int sum = (number * (number + 1) / 2);
+        System.out.println(sum);
+    }
+}
